b865bb526c2b11a0526d3370a22a8e8e05f370d4
Gabriel-sz-Aguiar <g.sz.aguiar@gmail.com>
2023-02-04 09:58:51 -0300

    formReact

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Formulario</h2>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
